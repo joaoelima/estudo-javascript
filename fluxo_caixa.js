955462400a9ec61f96984672f4137f2a0b44ac28
joaoedulima <106280952+joaoelima@users.noreply.github.com>
2022-06-26 23:44:07 -0300

    .

--- fluxo_caixa.js
+++ fluxo_caixa.js
@@ -10,11 +10,15 @@ e despesas e irá mostrar uma mensagem se a família está com
 saldo positivo ou negativo, seguido do valor do saldo.
 */
 
+
+//objeto com as informações de receita e despesa.
 let family = {
     incomes: [2500.00, 3200.00, 250.60, 360.45],
     expenses: [320.34, 128.45, 176.87, 1450.00]
 }
 
+
+//função onde será feita a soma.
 function sum(array) {
     let total = 0;
 
@@ -25,7 +29,7 @@ function sum(array) {
     return total
 }
 
-
+//Função que irá receber a soma feita na função SUM, atualizará os valores e mostrará no console.log.
 function calculateBalance() {
     const calculateIncomes = sum(family.incomes)
     const calculateExpenses = sum(family.expenses)
