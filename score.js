d9dc4b06bfbd413efd9d4ba1a9b74ecd74dfd13c
joaoedulima <106280952+joaoelima@users.noreply.github.com>
2022-06-26 22:58:22 -0300

    Utilizando boas praticas, deixando apenas um console.log no final.

--- score.js
+++ score.js
@@ -9,22 +9,27 @@
    * *menor que 60 - F
 */
 
-let score = 55
+let score = 42
 let scoreA = score >= 90 && score <= 100
 let scoreB = score >= 80 && score <= 89
 let scoreC = score >= 70 && score <= 79
 let scoreD = score >= 60 && score <= 69
-let scoreF = score <60
+let scoreF = score <60 && score >= 0
 
+let scoreFinal;
 
 if (scoreA) {
-    console.log("A")
+   scoreFinal = "A"
 } else if (scoreB) {
-    console.log("B")
+    scoreFinal = "B"
 } else if (scoreC) {
-    console.log("C")
+    scoreFinal = "C"
 } else if (scoreD) {
-    console.log("D")
+    scoreFinal = "D"
 } else if (scoreF) {
-    console.log("F")
+    scoreFinal = "F"
+} else {
+    scoreFinal = "Nota inválida"
 }
+
+console.log(scoreFinal)
