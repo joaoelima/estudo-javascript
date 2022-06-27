7c58aac57098cc3e9c83b9a6c956cf21afbe4f61
joaoedulima <106280952+joaoelima@users.noreply.github.com>
2022-06-27 00:48:26 -0300

    Iniciando um novo estudo.

---
+++ dados_array.js
@@ -0,0 +1,17 @@
+/* ### Buscando e contando dados em Arrays ### 
+
+   Baseado no Array de livros por categoria abaixo, faça os seguintes desafios.
+   
+   * Contar o numero de catagorias e o numero de livros em cada categoria.
+   * Contar o numero de autores.
+   * Mostrar livros do autor Augusto Cury.
+   * Transformar a função acima em uma função que ir[a receber o nome do autor e
+   devolver os livros desse autor.
+*/
+
+const booksByCategory = [
+    {
+        category: 'Riqueza',
+        books: [ ],
+    }
+]
