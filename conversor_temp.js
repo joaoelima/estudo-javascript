60b03d1d0482a3ab92cf4f8abd3d6a91d8f33bac
joaoedulima <106280952+joaoelima@users.noreply.github.com>
2022-06-27 00:41:30 -0300

    ;

--- conversor_temp.js
+++ conversor_temp.js
@@ -9,19 +9,34 @@
 
  */
 
+
+    //função que irá testar se a nomenclatura está correta, e tambem aceitará maiusculo ou minusculo. 
     function transformDegree(degree){
         const celsiusExists = degree.toUpperCase().includes('C')
         const fahrenheitExists = degree.toUpperCase().includes('F')
         
         
         if(!celsiusExists && !fahrenheitExists){
-            throw new Error('Grau não identificado')
+            throw new Error('Grau não identificado') //throw criado para verificar se a temperatura informada é valida.
         }
+        
+        //Fluxo para converção de F -> C
+        let updatedDegree = Number(degree.toUpperCase().replace("F", "")); 
+        let formula = fahrenheit => (fahrenheit - 32) * 5/9
+        let degreeSign = '°C'
+        
+        if(celsiusExists){
+            updatedDegree = Number(degree.toUpperCase().replace("C", "")); 
+            formula = celsius => celsius * 9/5 + 32
+            degreeSign = '°F'
+        }
+        
+        return formula(updatedDegree) + degreeSign
     }
     
     try {
-        transformDegree('50F')
-        transformDegree('50Z')
+       console.log(transformDegree('50C')) 
+       
     } catch (error) {
         console.log(error.message)
-    }    
+    }
