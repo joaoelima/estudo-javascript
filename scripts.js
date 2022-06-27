b4d2d39324db41b4b282e44afaa37ed1e2e80010
joaoedulima <106280952+joaoelima@users.noreply.github.com>
2022-06-26 22:32:24 -0300

    .

---
+++ scripts.js
@@ -0,0 +1,16 @@
+/* ### Transformar notas escolares
+   Crie um algoritmo que transforma as notas do sistema
+   numerico, para sistema de notas em caracteres tipo A B C .
+   
+   * de 90 para cima - A
+   * entre 80 - 89 - B
+   * entre 70 - 79 - C
+   * entre 60 - 69 - D
+   * *menor que 60 - F
+*/
+
+let score = 90
+
+if (score >= 90) {
+    console.log(score)
+}   
